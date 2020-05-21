e812186aee66a7d6e1252a104e26589339126fed
Yap Jing Kang <jkyap99@gmail.com>
2020-05-21 10:49:56 +0800

    Updates

---
+++ precache-manifest.86bf377d9a80eea1fc6d2df06f2c1e69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d136b2f5eb291097267eaf61beaea3",
+    "url": "/mern-tutorial/index.html"
+  },
+  {
+    "revision": "2c73368bed229092571c",
+    "url": "/mern-tutorial/static/css/main.eb3e390a.chunk.css"
+  },
+  {
+    "revision": "ac17413a91e4204658eb",
+    "url": "/mern-tutorial/static/js/2.9cb043cb.chunk.js"
+  },
+  {
+    "revision": "2c73368bed229092571c",
+    "url": "/mern-tutorial/static/js/main.627fd481.chunk.js"
+  },
+  {
+    "revision": "5c11e95ec32934e47cc2",
+    "url": "/mern-tutorial/static/js/runtime-main.5629f23c.js"
+  }
+]);
